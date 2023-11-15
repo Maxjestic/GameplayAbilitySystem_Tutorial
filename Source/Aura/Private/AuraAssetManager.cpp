eef6fbbc5599d45ebfe760240765e9c2af0cd1b4
Maksymilian Kafka <m.kafka00@gmail.com>
2023-11-15 17:51:31 +0100

    AssetManager class

---
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -0,0 +1,21 @@
+// Copyright Maxjestic
+
+
+#include "AuraAssetManager.h"
+
+#include "AuraGameplayTags.h"
+
+UAuraAssetManager& UAuraAssetManager::Get()
+{
+	check(GEngine)
+
+	UAuraAssetManager* AuraAssetManager = Cast<UAuraAssetManager>(GEngine->AssetManager);
+	return *AuraAssetManager;
+}
+
+void UAuraAssetManager::StartInitialLoading()
+{
+	Super::StartInitialLoading();
+
+	FAuraGameplayTags::InitializeNativeGameplayTags();
+}
