6212ecb779a99b719b85788ad43eb235cb37d7f5
Maksymilian Kafka <m.kafka00@gmail.com>
2024-01-14 18:15:45 +0100

    Spawning rock projectile

--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -6,6 +6,7 @@
 #include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
 #include "NiagaraFunctionLibrary.h"
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
 #include "Aura/Aura.h"
 #include "Components/AudioComponent.h"
 #include "Components/SphereComponent.h"
@@ -65,7 +66,10 @@ void AAuraProjectile::OnSphereOverlap( UPrimitiveComponent* OverlappedComponent,
 	{
 		return;
 	}
-
+	if (!UAuraAbilitySystemLibrary::IsNotFriend( DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser(), OtherActor ))
+	{
+		return;
+	}
 	if (!bHit)
 	{
 		UGameplayStatics::PlaySoundAtLocation( this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator );
