b2be6284667b8fe7c5beab308770e098edb36192
Maksymilian Kafka <m.kafka00@gmail.com>
2024-01-23 19:48:48 +0100

    Summon Ability - Drawing spheres in random order, Line Trace to get location on the ground

--- Source/Aura/Private/AbilitySystem/Ability/AuraSummonAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Ability/AuraSummonAbility.cpp
@@ -3,8 +3,6 @@
 
 #include "AbilitySystem/Ability/AuraSummonAbility.h"
 
-#include "Kismet/KismetSystemLibrary.h"
-
 TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
 {
 	const FVector Location = GetAvatarActorFromActorInfo()->GetActorLocation();
@@ -13,21 +11,25 @@ TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
 
 	const FVector LeftOfSpread = Forward.RotateAngleAxis( -SpawnSpreadAngle / 2.f, FVector::UpVector );
 	TArray<FVector> SpawnLocations;
+
 	for (int32 i = 0; i < NumMinionsToSpawn; i++)
 	{
 		const FVector Direction = LeftOfSpread.RotateAngleAxis( DeltaSpread * i, FVector::UpVector );
-		const FVector ChosenSpawnLocation = Location + Direction * FMath::FRandRange( MinSpawnDistance, MaxSpawnDistance );
-		SpawnLocations.Add(ChosenSpawnLocation);
-		DrawDebugSphere( GetWorld(), ChosenSpawnLocation, 18.f, 12, FColor::Cyan, false, 3.f );
+		FVector ChosenSpawnLocation = Location + Direction * FMath::FRandRange( MinSpawnDistance, MaxSpawnDistance );
 
-		UKismetSystemLibrary::DrawDebugArrow( GetAvatarActorFromActorInfo(),
-											  Location + Direction * MinSpawnDistance,
-											  Location + Direction * MaxSpawnDistance,
-											  4.f,
-											  FLinearColor::Green,
-											  3.f );
-		DrawDebugSphere( GetWorld(), Location + Direction * MinSpawnDistance, 5.f, 12, FColor::Red, false, 3.f );
-		DrawDebugSphere( GetWorld(), Location + Direction * MaxSpawnDistance, 5.f, 12, FColor::Red, false, 3.f );
+		FHitResult HitResult;
+		GetWorld()->LineTraceSingleByChannel( HitResult,
+		                                      ChosenSpawnLocation + FVector( 0.f, 0.f, 400.f ),
+		                                      ChosenSpawnLocation - FVector( 0.f, 0.f, 400.f ),
+		                                      ECC_Visibility );
+
+		if(HitResult.bBlockingHit)
+		{
+			ChosenSpawnLocation = HitResult.ImpactPoint;
+		}
+
+		SpawnLocations.Add( ChosenSpawnLocation );
 	}
+
 	return SpawnLocations;
 }
