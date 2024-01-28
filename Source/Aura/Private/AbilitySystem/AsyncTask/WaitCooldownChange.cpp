f1ec1def42c35f84a71f92354d04e92e1035c1d5
Maksymilian Kafka <m.kafka00@gmail.com>
2024-01-29 00:16:43 +0100

    Added custom BlueprintAsyncTask class to broadcast when ability cooldown starts and ends

---
+++ Source/Aura/Private/AbilitySystem/AsyncTask/WaitCooldownChange.cpp
@@ -0,0 +1,87 @@
+// Copyright Maxjestic
+
+
+#include "AbilitySystem/AsyncTask/WaitCooldownChange.h"
+
+#include "AbilitySystemComponent.h"
+#include "Aura/AuraLogChannels.h"
+
+UWaitCooldownChange* UWaitCooldownChange::WaitForCooldownChange( UAbilitySystemComponent* InAbilitySystemComponent,
+                                                                 const FGameplayTag& InCooldownTag )
+{
+	UWaitCooldownChange* WaitCooldownChange = NewObject<UWaitCooldownChange>();
+	WaitCooldownChange->AbilitySystemComponent = InAbilitySystemComponent;
+	WaitCooldownChange->CooldownTag = InCooldownTag;
+
+	if (!IsValid( InAbilitySystemComponent ) || !(InCooldownTag.IsValid()))
+	{
+		WaitCooldownChange->EndTask();
+		return nullptr;
+	}
+
+	// To know when Cooldown Gameplay Effect has been applied - cooldown started
+	InAbilitySystemComponent->OnActiveGameplayEffectAddedDelegateToSelf.AddUObject(
+		WaitCooldownChange,
+		&UWaitCooldownChange::OnActiveEffectAdded );
+
+	// To know when Cooldown Gameplay Tag has been remove - cooldown ended
+	InAbilitySystemComponent->RegisterGameplayTagEvent( InCooldownTag, EGameplayTagEventType::NewOrRemoved ).AddUObject(
+		WaitCooldownChange,
+		&UWaitCooldownChange::CooldownTagChanged );
+
+	return WaitCooldownChange;
+}
+
+void UWaitCooldownChange::EndTask()
+{
+	if (!IsValid( AbilitySystemComponent ))
+	{
+		return;
+	}
+
+	AbilitySystemComponent->RegisterGameplayTagEvent( CooldownTag, EGameplayTagEventType::NewOrRemoved ).RemoveAll( this );
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+void UWaitCooldownChange::OnActiveEffectAdded( UAbilitySystemComponent* InAbilitySystemComponent, const FGameplayEffectSpec& AppliedSpec,
+											   FActiveGameplayEffectHandle ActiveEffectHandle )
+{
+	FGameplayTagContainer AssetTags;
+	AppliedSpec.GetAllAssetTags( AssetTags );
+
+	FGameplayTagContainer GrantedTags;
+	AppliedSpec.GetAllAssetTags( GrantedTags );
+
+	if( !AssetTags.HasTagExact( CooldownTag ) || !GrantedTags.HasTagExact( CooldownTag ))
+	{
+		return;
+	}
+
+	const FGameplayEffectQuery GameplayEffectQuery = FGameplayEffectQuery::MakeQuery_MatchAnyOwningTags( CooldownTag.GetSingleTagContainer() );
+	TArray<float> TimesRemaining = AbilitySystemComponent->GetActiveEffectsTimeRemaining( GameplayEffectQuery );
+
+	if(TimesRemaining.IsEmpty())
+	{
+		return;
+	}
+
+	float TimeRemaining = TimesRemaining[0];
+	for(int32 i = 0; i < TimesRemaining.Num(); i++)
+	{
+		if(TimeRemaining < TimesRemaining[i])
+		{
+			TimeRemaining = TimesRemaining[i];
+		}
+	}
+
+	CooldownStart.Broadcast( TimeRemaining );
+}
+
+void UWaitCooldownChange::CooldownTagChanged( const FGameplayTag InCooldownTag, int32 NewCount )
+{
+	if (NewCount == 0)
+	{
+		CooldownEnd.Broadcast( 0.f );
+	}
+}
---
+++ Source/Aura/Public/AbilitySystem/AsyncTask/WaitCooldownChange.h
@@ -0,0 +1,58 @@
+// Copyright Maxjestic
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ActiveGameplayEffectHandle.h"
+#include "GameplayTagContainer.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "WaitCooldownChange.generated.h"
+
+struct FGameplayEffectSpec;
+class UAbilitySystemComponent;
+
+/** Broadcasts cooldown duration */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam( FCooldownChangeSignature, float, TimeRemaining );
+
+/**
+ * Waits for cooldown
+ * Informs when cooldown begins and ends
+ */
+UCLASS()
+class AURA_API UWaitCooldownChange : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+	/** Executed when cooldown starts */
+	UPROPERTY( BlueprintAssignable )
+	FCooldownChangeSignature CooldownStart;
+
+	/** Executed when cooldown ends */
+	UPROPERTY( BlueprintAssignable )
+	FCooldownChangeSignature CooldownEnd;
+
+	/** Factory that construct this */
+	UFUNCTION( BlueprintCallable, meta = (BlueprintInternalUseOnly = "true") )
+	static UWaitCooldownChange* WaitForCooldownChange( UAbilitySystemComponent* InAbilitySystemComponent,
+	                                                   const FGameplayTag& InCooldownTag );
+
+	/** Cleans up any resources when task is done, widget destroyed, ability system component or gameplay tag are not valid */
+	UFUNCTION( BlueprintCallable )
+	void EndTask();
+
+protected:
+	/** Stores received ability system component */
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	/** Stores received gameplay tag */
+	FGameplayTag CooldownTag;
+
+	/** Responses to OnActiveGameplayEffectAddedDelegateToSelf */
+	void OnActiveEffectAdded( UAbilitySystemComponent* InAbilitySystemComponent, const FGameplayEffectSpec& AppliedSpec,
+	                          FActiveGameplayEffectHandle ActiveEffectHandle );
+
+	/** Responses to cooldown gameplay tag change event */
+	void CooldownTagChanged( const FGameplayTag InCooldownTag, int32 NewCount );
+};
