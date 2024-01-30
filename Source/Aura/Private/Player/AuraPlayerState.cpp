6d116948209ed1d492627f113a8dbc8270b9eb12
Maksymilian Kafka <m.kafka00@gmail.com>
2024-01-30 20:53:56 +0100

    Added Experience variable (+Accessors,Add and OnRep Functions, Delegate), added lacking functionalities for Level variable

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -10,18 +10,47 @@ AAuraPlayerState::AAuraPlayerState()
 {
 	NetUpdateFrequency = 100.f;
 
-	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
-	AbilitySystemComponent->SetIsReplicated(true);
-	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>( "AbilitySystemComponent" );
+	AbilitySystemComponent->SetIsReplicated( true );
+	AbilitySystemComponent->SetReplicationMode( EGameplayEffectReplicationMode::Mixed );
 
-	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>( "AttributeSet" );
 }
 
 void AAuraPlayerState::GetLifetimeReplicatedProps( TArray<FLifetimeProperty>& OutLifetimeProps ) const
 {
-	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	Super::GetLifetimeReplicatedProps( OutLifetimeProps );
 
-	DOREPLIFETIME(AAuraPlayerState, Level)
+	DOREPLIFETIME( AAuraPlayerState, Level )
+	DOREPLIFETIME( AAuraPlayerState, Experience )
+}
+
+void AAuraPlayerState::SetLevel( const int32 InLevel )
+{
+	Level = InLevel;
+	Level = FMath::Clamp( Level, 0, Level );
+	OnLevelChangedDelegate.Broadcast( Level );
+}
+
+void AAuraPlayerState::AddToLevel( int32 InLevel )
+{
+	InLevel = FMath::Clamp( InLevel, 0, InLevel );
+	Level += InLevel;
+	OnLevelChangedDelegate.Broadcast( Level );
+}
+
+void AAuraPlayerState::SetExperience( const int32 InExperience )
+{
+	Experience = InExperience;
+	Experience = FMath::Clamp( Experience, 0, Experience );
+	OnExperienceChangedDelegate.Broadcast( Experience );
+}
+
+void AAuraPlayerState::AddToExperience( int32 InExperience )
+{
+	InExperience = FMath::Clamp( InExperience, 0, InExperience );
+	Experience += InExperience;
+	OnExperienceChangedDelegate.Broadcast( Experience );
 }
 
 UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
@@ -31,4 +60,9 @@ UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
 
 void AAuraPlayerState::OnRep_Level( int32 OldLevel )
 {
+}
+
+void AAuraPlayerState::OnRep_Experience( int32 OldExperience )
+{
+	OnExperienceChangedDelegate.Broadcast( Experience );
 }
--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -9,6 +9,9 @@
 
 class UAttributeSet;
 
+/** Broadcasts single value related to player stats */
+DECLARE_MULTICAST_DELEGATE_OneParam( FOnPlayerStatChanged, int32 /*StatValue*/ );
+
 /**
  * Player State class used as default player state in game
  */
@@ -22,27 +25,51 @@ public:
 	 * Sets NetUpdateFrequency, AbilitySystemComponent (+replication), AttributeSet
 	 */
 	AAuraPlayerState();
-	
+
 	//~ Begin UObject Interface
 	virtual void GetLifetimeReplicatedProps( TArray<FLifetimeProperty>& OutLifetimeProps ) const override;
 	//~ End UObject Interface
 
 	/** AttributeSet getter */
 	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
-	
-	/** Player level getter */ 
+
+
+	/** Player level getter */
 	FORCEINLINE int32 GetPlayerLevel() const { return Level; }
+
+	/** Sets Level to given value */
+	void SetLevel( const int32 InLevel );
+
+	/** Adds given number to current Level */
+	void AddToLevel( int32 InLevel );
+
+	/** Broadcast Experience amount on change */
+	FOnPlayerStatChanged OnLevelChangedDelegate;
+
+
+	/** Player Experience getter */
+	FORCEINLINE int32 GetPlayerExperience() const { return Experience; }
+
+	/** Sets Experience to given amount */
+	void SetExperience( int32 InExperience );
+
+	/** Adds given amount to current experience amount */
+	void AddToExperience( int32 InExperience );
+
+	/** Broadcast Experience amount on change */
+	FOnPlayerStatChanged OnExperienceChangedDelegate;
+
 
 	//~ Begin IAbilitySystem Interface
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 	//~ End IAbilitySystem Interface
-	
+
 protected:
 	/**
 	 * AbilitySystemComponent owned by player character
 	 * It persists after destruction of the player-controlled pawn and can be reused after creating new one
 	 */
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY( VisibleAnywhere )
 	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
 
 	/**
@@ -53,11 +80,19 @@ protected:
 	TObjectPtr<UAttributeSet> AttributeSet;
 
 private:
-	/** Player level */
-	UPROPERTY(VisibleAnywhere, ReplicatedUsing = OnRep_Level)
+	/** Player level based on experience points */
+	UPROPERTY( VisibleAnywhere, ReplicatedUsing = OnRep_Level )
 	int32 Level = 1;
+
+	/** Total amount of experience Points, does not reset on level up */
+	UPROPERTY( VisibleAnywhere, ReplicatedUsing = OnRep_Experience )
+	int32 Experience = 0;
 
 	/** Level replication function */
 	UFUNCTION()
 	void OnRep_Level( int32 OldLevel );
+
+	/** Experience replication function */
+	UFUNCTION()
+	void OnRep_Experience( int32 OldExperience );
 };
