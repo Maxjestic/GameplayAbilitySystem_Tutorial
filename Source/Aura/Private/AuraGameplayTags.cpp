aea07eed8a945e5f320cde1cc92254bb0072e560
Maksymilian Kafka <m.kafka00@gmail.com>
2024-02-08 00:21:30 +0100

    Added WBP_EquippedSpellRow_Button, Setting equipped spell buttons icon and background based on input tags

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -105,6 +105,14 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 		FName( "InputTag.4" ),
 		FString( "Input Tag for 4 key" ) );
 
+	GameplayTags.InputTag_Passive_1 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName( "InputTag.Passive.1" ),
+		FString( "Input Tag for Passive Ability 1" ) );
+
+	GameplayTags.InputTag_Passive_2 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName( "InputTag.Passive.2" ),
+		FString( "Input Tag for Passive Ability 2" ) );
+
 	/**
 	 * Damage Type Tags
 	 */
