a628737c2bb34f2f65d7edef27ea44186d2224e5
Maksymilian Kafka <m.kafka00@gmail.com>
2024-03-12 21:28:55 +0100

    Arcane Shards - Added MagicCircle class

---
+++ Source/Aura/Private/Actor/MagicCircle.cpp
@@ -0,0 +1,24 @@
+// Copyright Maxjestic
+
+
+#include "Actor/MagicCircle.h"
+
+#include "Components/DecalComponent.h"
+
+AMagicCircle::AMagicCircle()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	MagicCircleDecal = CreateDefaultSubobject<UDecalComponent>( "MagicCircleDecal" );
+	MagicCircleDecal->SetupAttachment( GetRootComponent() );
+}
+
+void AMagicCircle::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AMagicCircle::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+}
---
+++ Source/Aura/Public/Actor/MagicCircle.h
@@ -0,0 +1,33 @@
+// Copyright Maxjestic
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MagicCircle.generated.h"
+
+/*
+ * class for arcane shards ability’s magic circle
+ */
+UCLASS()
+class AURA_API AMagicCircle : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	/** Default constructor */
+	AMagicCircle();
+	
+	//~ Begin AActor Interface
+	virtual void Tick(float DeltaTime) override;
+	//~ End AActor Interface
+
+protected:
+	//~ Begin AActor Interface
+	virtual void BeginPlay() override;
+	//~ End AActor Interface
+
+	/** Decal component for magic circle */
+	UPROPERTY( VisibleAnywhere)
+	TObjectPtr<UDecalComponent> MagicCircleDecal;
+};
