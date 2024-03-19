9e4063f19f71caa7b0c610e3b073865e2c435589
Maksymilian Kafka <m.kafka00@gmail.com>
2024-03-19 20:33:24 +0100

    New collision channel for mouse trace

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -11,6 +11,7 @@
 #include "NiagaraFunctionLibrary.h"
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "Actor/MagicCircle.h"
+#include "Aura/Aura.h"
 #include "Components/DecalComponent.h"
 #include "Components/SplineComponent.h"
 #include "GameFramework/Character.h"
@@ -170,7 +171,9 @@ void AAuraPlayerController::CursorTrace()
 		NewActor = nullptr;
 		return;
 	}
-	GetHitResultUnderCursor( ECC_Visibility, false, CursorHit );
+
+	const ECollisionChannel TraceChannel = IsValid(MagicCircle) ? ECC_ExcludePlayers : ECC_Visibility;
+	GetHitResultUnderCursor( TraceChannel, false, CursorHit );
 	if (!CursorHit.bBlockingHit) return;
 
 	PreviousActor = NewActor;
